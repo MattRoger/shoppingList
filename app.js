2edcc0cc1c04c7d94d021e6dbedce966d648c9aa
MattRoger <mrogerphotography@gmail.com>
2020-12-20 15:31:59 -0500

    added input field

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("working")
+
+const addItemInput = document.querySelector('input.addItemInput')
+const addItemInputQuantity = document.querySelector('input.addItemInputQuantity')
+const addItemButton = document.querySelector('button.addItemButton');
+
+
+addItemButton.addEventListener('click', ()=>{
+    let ul=document.getElementsByTagName('ul')[0];
+    let li= document.createElement('li');
+    li.textContent=addItemInputQuantity.value +" " + addItemInput.value
+    ul.appendChild(li)
+})
