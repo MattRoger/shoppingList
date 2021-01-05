cb32dbd7ea1c321d2328ee32c19c948ceddcb672
MattRoger <mrogerphotography@gmail.com>
2021-01-04 21:02:49 -0500

    added styles, when checkbox clicked, changes text

--- app.js
+++ app.js
@@ -13,14 +13,16 @@ let addItemInputQuantity = document.querySelector(
 const addItemButton = document.querySelector("button.addItemButton");
 const listUl = listDiv.querySelector("ul");
 const list = listUl.children;
-const buttons= list.siblings
 
 
 function attachListItemsButtons(li) {
   let checkbox = document.createElement("input");
   checkbox.className = "checkbox";
- checkbox.type="checkbox"
+  checkbox.type="checkbox";
   li.appendChild(checkbox);
+  let label= document.createElement("label");
+  label.textContent="Need to Purchase";
+  li.appendChild(label)
   let up = document.createElement("button");
   up.className = "up";
   up.textContent = "up";
@@ -92,10 +94,14 @@ listUl.addEventListener("mouseout", (e) => {
   });
 
 
+
 addItemButton.addEventListener("click", () => {
   let ul = document.getElementsByTagName("ul")[0];
   let li = document.createElement("li");
-  li.textContent = addItemInputQuantity.value + " " + addItemInput.value;
+  let p = document.createElement("p");
+  p.style.display="inline";
+  p.textContent = addItemInputQuantity.value + "x" + " " + addItemInput.value;
+  li.appendChild(p);
   attachListItemsButtons(li);
   ul.appendChild(li);
   li.style.border="1px solid black";
@@ -104,3 +110,21 @@ addItemButton.addEventListener("click", () => {
   addItemInputQuantity.value=1;
 });
 
+listUl.onchange=function(){
+    let checkbox= document.getElementsByClassName("checkbox");
+    for(let i=0; i<checkbox.length; i++){
+        let li =checkbox[i].previousElementSibling
+        let label =checkbox[i].nextElementSibling
+        if(checkbox[i].checked==true){
+            li.style.opacity=".5";
+            label.textContent="Purchased"
+        }else{
+            li.style.opacity=1;
+            label.textContent="need to Purchase"
+        }
+        console.log(checkbox[i].checked)
+
+    }
+    
+   
+}
